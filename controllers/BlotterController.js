0087b4eb2b9c34b77171d995caf498b340ab93ad
Kenneth Jay Bruno <61215678+kjdbruno@users.noreply.github.com>
2025-07-20 19:21:47 +0800

    blotters
    all blotter

--- controllers/BlotterController.js
+++ controllers/BlotterController.js
@@ -6,7 +6,8 @@ const {
     BlotterHandler, 
     BlotterAction, 
     BlotterAttachment, 
-    BlotterActionAttachment
+    BlotterActionAttachment,
+    Resident
 } = require('../models');
 
 const fs = require('fs');
@@ -37,7 +38,6 @@ exports.createBlotter = async (req, res) => {
         Location, 
         Description 
     } = req.body;
-    const files = req.files; // From Multer
     try {
         
         const currentYear = new Date().getFullYear();
@@ -56,7 +56,7 @@ exports.createBlotter = async (req, res) => {
             CaseNo = `BLT${currentYear}-0001`;
         } else {
             const lastNo = parseInt(lastBlotter.CaseNo.split('-')[1]);
-            CaseNo = `BLT-${String(lastNo + 1).padStart(4, '0')}`;
+            CaseNo = `BLT${currentYear}-${String(lastNo + 1).padStart(4, '0')}`;
         }
 
         const blotter = await Blotter.create({
@@ -66,25 +66,6 @@ exports.createBlotter = async (req, res) => {
             Location,
             Description
         });
-
-        if (files && files.length > 0) {
-
-            for (const file of files) {
-                const filename = `${CaseNo}-${Date.now()}${path.extname(file.originalname)}`;
-                const uploadPath = path.join(__dirname, '../public/uploads/attachments', filename);
-
-                await sharp(file.buffer)
-                    .resize({ width: 800 })
-                    .jpeg({ quality: 80 })
-                    .toFile(uploadPath);
-
-                // Save each to DB or just collect for later use
-                await BlotterAttachment.create({
-                    BlotterId: blotter.Id,
-                    Filename: filename,
-                });
-            }
-        }
 
         res.status(201).json({ message: "Blotter created successfully.", blotter });
     } catch (error) {
@@ -101,7 +82,7 @@ exports.updateBlotter = async (req, res) => {
         Location, 
         Description 
     } = req.body;
-    const Files = req.Files; // From Multer
+    const files = req.files; // From Multer
   
     try {
         const blotter = await Blotter.findByPk(id);
@@ -122,23 +103,6 @@ exports.updateBlotter = async (req, res) => {
             Location,
             Description
         });
-
-        if (Files && Files.length > 0) {
-            for (const file of Files) {
-                const filename = `${blotter.CaseNo}-${Date.now()}-${Math.floor(Math.random() * 1000)}${path.extname(file.originalname)}`;
-                const uploadPath = path.join(__dirname, '../public/uploads/attachments', filename);
-
-                await sharp(file.buffer)
-                    .resize({ width: 800 })
-                    .jpeg({ quality: 80 })
-                    .toFile(uploadPath);
-
-                await BlotterAttachment.create({
-                    BlotterId: blotter.Id,
-                    Filename: filename,
-                });
-            }
-        }
 
         res.status(200).json({ message: "Blotter updated successfully.", blotter });
     } catch (error) {
@@ -173,6 +137,389 @@ exports.enableBlotter = async (req, res) => {
         }
         await blotter.update({ IsActive: true });
         res.status(200).json({ message: "Blotter enabled successfully.", blotter });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getAllBlotterParties = async (req, res) => {
+
+    const { id } = req.params;
+
+    try {
+        const blotters = await BlotterParty.findAll({
+            where: {
+                BlotterId: id
+            },
+            include: [
+                {
+                    model: Resident,
+                    as: 'resident'
+                }
+            ]
+        })
+        res.json(blotters);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.createBlotterParty = async (req, res) => {
+    const { 
+        Firstname,
+        Middlename,
+        Lastname,
+        Suffix,
+        BlotterId,
+        ResidentId,
+        Role,
+        Statement
+    } = req.body;
+    try {
+        let residentId = ResidentId;
+        // check Firstname, Middlename, and Lastname has value
+        if (Firstname && Middlename && Lastname) {
+
+            // check fullname if exist
+
+
+            const resident = await Resident.create({
+                Firstname,
+                Middlename,
+                Lastname,
+                Suffix,
+                IsResident: false
+            });
+            residentId = resident.Id;
+        }
+
+        // check if party exist
+        
+        const blotter = await BlotterParty.create({
+            BlotterId,
+            ResidentId: residentId,
+            Role,
+            Statement
+        });
+
+        res.status(201).json({ message: "Blotter party created successfully.", blotter });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+exports.disableBlotterParty = async (req, res) => {
+
+    const { id } = req.params;
+  
+    try {
+        const blotter = await BlotterParty.findByPk(id);
+        if (!blotter) {
+            return res.status(404).json({ error: "Blotter party not found." });
+        }
+        await blotter.update({ IsActive: false });
+        res.status(200).json({ message: "Blotter party disabled successfully.", blotter });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.enableBlotterParty = async (req, res) => {
+
+    const { id } = req.params;
+  
+    try {
+        const blotter = await BlotterParty.findByPk(id);
+        if (!blotter) {
+            return res.status(404).json({ error: "Blotter party not found." });
+        }
+        await blotter.update({ IsActive: true });
+        res.status(200).json({ message: "Blotter party enabled successfully.", blotter });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getAllBlotterHandlers = async (req, res) => {
+
+    const { id } = req.params;
+
+    try {
+        const blotters = await BlotterHandler.findAll({
+            where: {
+                BlotterId: id
+            },
+            include: [
+                {
+                    model: Resident,
+                    as: 'resident'
+                }
+            ]
+        });
+        res.json(blotters);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.createBlotterHandler = async (req, res) => {
+    const { 
+        BlotterId,
+        OfficialId,
+        Role
+    } = req.body;
+    try {
+        // check if handler exist
+
+
+        const blotter = await BlotterHandler.create({
+            BlotterId,
+            OfficialId,
+            Role
+        });
+
+        res.status(201).json({ message: "Blotter handler created successfully.", blotter });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+exports.disableBlotterHandler = async (req, res) => {
+
+    const { id } = req.params;
+  
+    try {
+        const blotter = await BlotterHandler.findByPk(id);
+        if (!blotter) {
+            return res.status(404).json({ error: "Blotter handler not found." });
+        }
+        await blotter.update({ IsActive: false });
+        res.status(200).json({ message: "Blotter handler disabled successfully.", blotter });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.enableBlotterHandler = async (req, res) => {
+
+    const { id } = req.params;
+  
+    try {
+        const blotter = await BlotterHandler.findByPk(id);
+        if (!blotter) {
+            return res.status(404).json({ error: "Blotter handler not found." });
+        }
+        await blotter.update({ IsActive: true });
+        res.status(200).json({ message: "Blotter handler enabled successfully.", blotter });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getAllBlotterActions = async (req, res) => {
+
+    const { id } = req.params;
+
+    try {
+        const blotters = await BlotterAction.findAll({
+            where: {
+                BlotterId: id
+            }
+        });
+        res.json(blotters);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.createBlotterAction = async (req, res) => {
+    const { 
+        BlotterId,
+        Action,
+        Date,
+        Remarks
+    } = req.body;
+    try {
+        const blotter = await BlotterAction.create({
+            BlotterId,
+            Action,
+            Date,
+            Remarks
+        });
+
+        res.status(201).json({ message: "Blotter action created successfully.", blotter });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+exports.disableBlotterAction = async (req, res) => {
+
+    const { id } = req.params;
+  
+    try {
+        const blotter = await BlotterAction.findByPk(id);
+        if (!blotter) {
+            return res.status(404).json({ error: "Blotter action not found." });
+        }
+        await blotter.update({ IsActive: false });
+        res.status(200).json({ message: "Blotter action disabled successfully.", blotter });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.enableBlotterAction = async (req, res) => {
+
+    const { id } = req.params;
+  
+    try {
+        const blotter = await BlotterAction.findByPk(id);
+        if (!blotter) {
+            return res.status(404).json({ error: "Blotter action not found." });
+        }
+        await blotter.update({ IsActive: true });
+        res.status(200).json({ message: "Blotter action enabled successfully.", blotter });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getAllBlotterAttachments = async (req, res) => {
+
+    const { id } = req.params;
+
+    try {
+        const blotters = await BlotterAttachment.findAll({
+            where: {
+                BlotterId: id
+            }
+        });
+        res.json(blotters);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.createBlotterAttachment = async (req, res) => {
+    const { 
+        BlotterId
+    } = req.body;
+    const files = req.files; // From Multer
+    try {
+        if (files && files.length > 0) {
+
+            for (const file of files) {
+                const filename = `${BlotterId}-${Date.now()}-${Math.floor(Math.random() * 10000)}${path.extname(file.originalname)}`;
+                const uploadPath = path.join(__dirname, '../public/uploads/attachments', filename);
+
+                await sharp(file.buffer)
+                    .resize({ width: 800 })
+                    .jpeg({ quality: 80 })
+                    .toFile(uploadPath);
+
+                // Save each to DB or just collect for later use
+                await BlotterAttachment.create({
+                    BlotterId,
+                    Filename: filename,
+                });
+            }
+        }
+
+        res.status(201).json({ message: "Blotter attachment created successfully."});
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+exports.deleteBlotterAttachment = async (req, res) => {
+
+    const { id } = req.params;
+  
+    try {
+        const attachment = await BlotterAttachment.findByPk(id);
+        if (!attachment) {
+            return res.status(404).json({ message: 'Attachment not found.' });
+        }
+        
+        const filePath = path.join(__dirname, '../public/uploads/attachments', attachment.Filename);
+
+        if (fs.existsSync(filePath)) {
+            fs.unlinkSync(filePath);
+        }
+
+        await attachment.destroy();
+        res.status(200).json({ message: "Blotter attachment deleted successfully." });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getAllBlotterActionAttachments = async (req, res) => {
+
+    const { id } = req.params;
+
+    try {
+        const blotters = await BlotterActionAttachment.findAll({
+            where: {
+                BlotterActionId: id
+            }
+        });
+        res.json(blotters);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.createBlotterActionAttachment = async (req, res) => {
+    const { 
+        BlotterActionId
+    } = req.body;
+    const files = req.files; // From Multer
+    try {
+        if (files && files.length > 0) {
+
+            for (const file of files) {
+                const filename = `${BlotterActionId}-${Date.now()}-${Math.floor(Math.random() * 10000)}${path.extname(file.originalname)}`;
+                const uploadPath = path.join(__dirname, '../public/uploads/attachments', filename);
+
+                await sharp(file.buffer)
+                    .resize({ width: 800 })
+                    .jpeg({ quality: 80 })
+                    .toFile(uploadPath);
+
+                // Save each to DB or just collect for later use
+                await BlotterActionAttachment.create({
+                    BlotterActionId,
+                    Filename: filename,
+                });
+            }
+        }
+
+        res.status(201).json({ message: "Blotter attachment created successfully."});
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+exports.deleteBlotterActionAttachment = async (req, res) => {
+
+    const { id } = req.params;
+  
+    try {
+        const attachment = await BlotterActionAttachment.findByPk(id);
+        if (!attachment) {
+            return res.status(404).json({ message: 'Attachment not found.' });
+        }
+        
+        const filePath = path.join(__dirname, '../public/uploads/attachments', attachment.Filename);
+
+        if (fs.existsSync(filePath)) {
+            fs.unlinkSync(filePath);
+        }
+
+        await attachment.destroy();
+        res.status(200).json({ message: "Blotter attachment deleted successfully." });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
--- routes/BlotterRoutes.js
+++ routes/BlotterRoutes.js
@@ -6,7 +6,25 @@ const {
     createBlotter,
     updateBlotter,
     disableBlotter,
-    enableBlotter
+    enableBlotter,
+    getAllBlotterParties,
+    createBlotterParty,
+    disableBlotterParty,
+    enableBlotterParty,
+    getAllBlotterHandlers,
+    createBlotterHandler,
+    disableBlotterHandler,
+    enableBlotterHandler,
+    getAllBlotterActions,
+    createBlotterAction,
+    disableBlotterAction,
+    enableBlotterAction,
+    getAllBlotterAttachments,
+    createBlotterAttachment,
+    deleteBlotterAttachment,
+    getAllBlotterActionAttachments,
+    createBlotterActionAttachment,
+    deleteBlotterActionAttachment
  } = require('../controllers/BlotterController');
 
  const multer = require('multer');
@@ -14,9 +32,32 @@ const {
  const upload = multer({ storage });
 
 router.get('/', getAllBlotters);
-router.post('/', upload.array('files'), createBlotter);
-router.post('/:id/update', upload.array('files'), updateBlotter);
+router.post('/', createBlotter);
+router.post('/:id/update', updateBlotter);
 router.post('/:id/disable', disableBlotter);
 router.post('/:id/enable', enableBlotter);
+
+router.get('/party', getAllBlotterParties);
+router.post('/party', createBlotterParty);
+router.post('/party/:id/disable', disableBlotterParty);
+router.post('/party/:id/enable', enableBlotterParty);
+
+router.get('/handler', getAllBlotterHandlers);
+router.post('/handler', createBlotterHandler);
+router.post('/handler/:id/disable', disableBlotterHandler);
+router.post('/handler/:id/enable', enableBlotterHandler);
+
+router.get('/action', getAllBlotterActions);
+router.post('/action', createBlotterAction);
+router.post('/action/:id/disable', disableBlotterAction);
+router.post('/action/:id/enable', enableBlotterAction);
+
+router.get('/attachment', getAllBlotterAttachments);
+router.post('/attachment', upload.array('files'), createBlotterAttachment);
+router.post('/attachment/:id/delete', deleteBlotterAttachment);
+
+router.get('/action/attachment', getAllBlotterActionAttachments);
+router.post('/action/attachment', upload.array('files'), createBlotterActionAttachment);
+router.post('/action/attachment/:id/delete', deleteBlotterActionAttachment);
 
 module.exports = router;
